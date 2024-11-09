d15ca4e02e5c0bf1a8370a1f4e5ac2bb3f71f02c
Farxxq <farooqmohamed2807@gmail.com>
2024-11-09 23:15:31 +0530

    Day 3 Questions to be solved....... by tmrw

--- Python/PracticingConceptsDay3.py
+++ Python/PracticingConceptsDay3.py
@@ -4,3 +4,15 @@
 
 print("---------Strings questions-------")
 
+# 1. Print the string in reverse,in upper,in lower and copy into another string
+print("Print the string length and in reverse,in upper,in lower and copy into another string")
+
+string = "Hello this is Faroo"
+
+print(f"The string: {string}")
+print(f"The length of string: {len(string)}")
+
+#Reverse the string
+print("Reversing the string")
+revString = string[::-1]
+print(f"The actual string: {string},\nThe reversed string: {revString}")
