0a878c8581f40e0fb41b2e80daaacea16373126d
Farxxq <farooqmohamed2807@gmail.com>
2024-11-12 21:43:08 +0530

    Arrays and it's operations in python

---
+++ Python/Day4.py
@@ -0,0 +1,6 @@
+#Day 4
+
+## Arrays and it's operations
+print("-----------Arrays and it's operations----------")
+# Arrays are not a built-in data type but can be created using the array module or libraries like NumPy.
+
