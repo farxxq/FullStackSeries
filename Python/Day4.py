f763d628aed4029128a4d1b0ce82c624b80b1a01
Farxxq <farooqmohamed2807@gmail.com>
2024-11-13 17:49:49 +0530

    Practice question Day3

--- Python/Day4.py
+++ Python/Day4.py
@@ -4,3 +4,13 @@
 print("-----------Arrays and it's operations----------")
 # Arrays are not a built-in data type but can be created using the array module or libraries like NumPy.
 
+# To create an array we need to import the module first
+# The numpy can also be used to make the array 
+    #Syntax of it: np.array([1,2,3,4,5])
+
+from array import array
+
+myArray = array('i',[1,2,3,4])
+
+# The below code will display the value of the myArray and not the elements of the array.... We need to change it to list if we need only the elements to display
+print(f"myArray = {myArray} and the it's type is {type(myArray)}") 
--- Python/PracticingConceptsDay3.py
+++ Python/PracticingConceptsDay3.py
@@ -5,14 +5,39 @@
 print("---------Strings questions-------")
 
 # 1. Print the string in reverse,in upper,in lower and copy into another string
-print("Print the string length and in reverse,in upper,in lower and copy into another string")
+print("1. Print the string length and in reverse,in upper,in lower and copy into another string")
 
 string = "Hello this is Faroo"
 
 print(f"The string: {string}")
+
+#Length
+print("----Length of the string----")
 print(f"The length of string: {len(string)}")
 
 #Reverse the string
-print("Reversing the string")
-revString = string[::-1]
+print("----Reversing the string---")
+revString = string[::-1] #Indexing is used in the strings
 print(f"The actual string: {string},\nThe reversed string: {revString}")
+
+    #Using for loop
+forRevString = ''
+for i in range(len(string)-1,-1,-1):
+    forRevString += string[i]
+print("Reversing using for loop:-")
+print(f"The reverse string using for loop: {forRevString}")
+
+# lower string
+print("-----Lower string----")
+lowerString = string.lower()
+print(f"Lower string: {lowerString}")
+
+#Copy
+print("----Copy----")
+copyString = string
+print(f"Copied the string in new var: {copyString}")
+
+# 2. Arrange string characters such that lowercase letters should come first in another string.
+print("2. Arrange string characters such that lowercase letters should come first in another string.")
+
+arrangeString = ""
