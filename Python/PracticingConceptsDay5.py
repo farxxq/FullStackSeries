a87dc225798e9b4a29f235ea1b96074e110e3570
Farxxq <farooqmohamed2807@gmail.com>
2024-11-17 22:27:28 +0530

    Practicing concepts Day 5 Object oriented programming

--- Python/PracticingConceptsDay5.py
+++ Python/PracticingConceptsDay5.py
@@ -1,3 +1,43 @@
 # Practicing the concept of Day 5
-
 # Small project kinda using the OOPs concepts
+
+class Student:
+    def __init__(self,rrn,name,dept):
+        self.rrn = rrn
+        self.name = name
+        self.dept = dept
+        self.marks = {}
+
+    def add_marks(self,subject,marks):
+        if subject in self.marks:
+            print(f"This {subject} already has marks of {self.marks}")
+        else:
+            self.marks[subject] = marks
+
+    def avg(self):
+        if not self.marks:
+            print("There are no marks available")
+        total = sum(self.marks.values())
+        avg = total/len(self.marks)
+        return avg
+    
+    def display(self):
+        print(f"Rrn of: {self.rrn}")
+        print(f"Name: {self.name}")
+        print(f"dept: {self.dept}")
+        print(f"Marks: {self.marks}")
+        print(f"Average Marks obtained: {self.avg()}")
+        
+
+student1 = Student(96,"Faroo","CSE")
+student2 = Student(49,"S","Tech") 
+
+student1.add_marks("Full Stack",90)
+student1.add_marks("Ai Chatbot",88)
+
+student2.add_marks("Cancer Bio",99)
+student2.add_marks("Data Analytics",95)
+
+student1.display()
+student2.display()
+
