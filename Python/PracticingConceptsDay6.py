e6978ee571d676c06f5b261c7125d535a623d4e4
Farxxq <farooqmohamed2807@gmail.com>
2024-11-22 23:04:43 +0530

    Started Binary Search

--- Python/PracticingConceptsDay6.py
+++ Python/PracticingConceptsDay6.py
@@ -1,13 +1,17 @@
 # Searching Algorithms
 
-#Linear Search
-listSearch = [1,2,2004,28,7,2003]
+# Linear Search
+linearSearch = [1,2,2004,28,7,2003]
 n = int(input("Enter the element you want to search: "))
 index = 0
-for i in range(len(listSearch)):
-    if listSearch[i] == n:
+for i in range(len(linearSearch)):
+    if linearSearch[i] == n:
         index += i
     else:
         pass
 
 print(f"The element {n} is at index : {index}")
+
+# Binary Search
+
+binarySearch = [1,2,3,4,5,6,7]
