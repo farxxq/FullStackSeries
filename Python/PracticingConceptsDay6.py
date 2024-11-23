685c0129677cd8545d45e0bc25434fa99cb7b25a
Farxxq <farooqmohamed2807@gmail.com>
2024-11-23 16:45:32 +0530

    Sorting Algos

--- Python/PracticingConceptsDay6.py
+++ Python/PracticingConceptsDay6.py
@@ -1,17 +1,67 @@
-# Searching Algorithms
+## Sorting Algos
 
-# Linear Search
-linearSearch = [1,2,2004,28,7,2003]
-n = int(input("Enter the element you want to search: "))
-index = 0
-for i in range(len(linearSearch)):
-    if linearSearch[i] == n:
-        index += i
-    else:
-        pass
+#Bubble Sorting
+def BubbleSort(bubbleSort):
+    print("------Bubble Sorting------")
 
-print(f"The element {n} is at index : {index}")
+    for i in range(len(bubbleSort)):
+        isSwap = False # Used to optimize (If the list or the array is sorted it will not run the whole loop i :- (len(bubbleSort)) times)
 
-# Binary Search
+        for j in range(len(bubbleSort)-1):
 
-binarySearch = [1,2,3,4,5,6,7]
+            if bubbleSort[j] > bubbleSort[j+1]:
+                bubbleSort[j], bubbleSort[j+1] = bubbleSort[j+1],bubbleSort[j]
+                print(f"Swapped: {bubbleSort[j]} and {bubbleSort[j+1]}")
+                isSwap = True
+
+        if(not isSwap):
+            break
+
+        print(f"After Pass {i + 1}: {bubbleSort}")
+
+    print(f"Sorted List: {bubbleSort}")
+
+#Selection Sort
+def SelectionSort(a):
+    print("-----Selection Sort----------")
+    for i in range(0,len(a)-1):
+        smallIndex = i
+
+        for j in range(i+1,len(a)):
+            if a[j] < a[smallIndex]:
+                smallIndex = j
+        a[i], a[smallIndex] = a[smallIndex], a[i]
+        print(f"Pass:{i} \nthe List: {a}")
+
+
+
+
+unsortedList = [1,2,2004,28,7,2003]
+
+# BubbleSort(unsortedList)
+SelectionSort(unsortedList)
+
+
+
+
+
+
+
+
+# # Searching Algorithms
+
+# # Linear Search
+# linearSearch = [1,2,2004,28,7,2003]
+# n = int(input("Enter the element you want to search: "))
+# index = 0
+# for i in range(len(linearSearch)):
+#     if linearSearch[i] == n:
+#         index += i
+#     else:
+#         pass
+
+# print(f"The element {n} is at index : {index}")
+
+# # Binary Search
+
+# binarySearch = [1,2,3,4,5,6,7]
