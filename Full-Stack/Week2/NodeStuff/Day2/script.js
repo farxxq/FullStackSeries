7443c32bc981ab58b71e68731e642cafcf484e9e
Farxxq <farooqmohamed2807@gmail.com>
2024-11-29 20:12:47 +0530

    Day2

---
+++ Full-Stack/Week2/NodeStuff/Day2/script.js
@@ -0,0 +1,19 @@
+
+// # npm details
+
+// to install:
+// npm install package_name
+
+// to uninstall:
+// npm uninstall package_name
+
+// version installation:
+// npm i package_name@version
+
+// dependencies: - packages that has been taken from net (this consists of further packages or files)
+// npm install package_name
+
+// devDependencies: - packages that is just for the dev but when it gets deployed these aren't used
+// npm i package_name --save-dev
+
+console.log("Assalamwalaikum")
