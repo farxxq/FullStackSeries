9e8008616b95e16dd0ee16c4dc27e562e2498631
Farxxq <farooqmohamed2807@gmail.com>
2024-12-04 12:41:12 +0530

    Practical part of MongoDB

--- Full-Stack/Week3/MongoDB/Practicals/app.js
+++ Full-Stack/Week3/MongoDB/Practicals/app.js
@@ -7,6 +7,7 @@ app.get('/', (req,res)=>{
     res.send("hey")
 })
 
+// Create a user
 app.get('/create',async (req,res)=>{
     let createduser = await userModel.create(
         {
@@ -16,7 +17,62 @@ app.get('/create',async (req,res)=>{
         }
     )
     res.send(createduser)
-    console.log("User is created")
+    console.log("User is created",createduser)
+})
+
+app.get('/create',async (req,res)=>{
+    let createduser = await userModel.create(
+        {
+            name: "Love",
+            email: "love@gmail.com",
+            username: "Far"
+        }
+    )
+    res.send(createduser)
+    console.log("User is created",createduser)
+})
+
+// Read the documents or find the documents
+app.get('/read',async (req,res)=>{
+    let readuser = await userModel.find()
+    res.send(readuser)
+    console.log("Users are read",readuser)
+})
+
+// Read a specific document or find a specific document
+app.get('/readUser',async (req, res)=>{
+    let readOneUser = await userModel.find({name: "light"})
+    res.send(readOneUser)
+    console.log("Read one user:", readOneUser)
+})
+
+// Read the first occuring document
+app.get('/readOne',async (req, res)=>{
+    let readOneUser = await userModel.findOne({name: "Love"})
+    res.send(readOneUser)
+    console.log("Read one user:", readOneUser)
+})
+
+// Update the data of a user
+app.get('/update',async (req,res)=>{
+    let updateuser = await userModel.findOneAndUpdate(
+        {
+            username: "Faroo"
+        },{
+            name: "light"
+        },{
+            new: true
+        }
+    )
+    res.send(updateuser)
+    console.log("User is updated",updateuser)
+})
+
+// Delete the document
+app.get('/delete', async (req,res) =>{
+    let deleteUser = await userModel.findOneAndDelete({name: "Farooq"})
+    res.send(deleteUser)
+    console.log("The account is deleted:", deleteUser)
 })
 
 app.listen(3000)
--- Full-Stack/Week3/MongoDB/Practicals/usermodel.js
+++ Full-Stack/Week3/MongoDB/Practicals/usermodel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb://127.0.0.1:2707/mongoLove`)
+mongoose.connect(`mongodb://localhost:27017/MongoLove`)
 
 const userSchema = mongoose.Schema({
     name: String,
