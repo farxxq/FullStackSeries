0adfb30322c3ab2b79066c49caa9f208dc46c5b8
Farxxq <farooqmohamed2807@gmail.com>
2024-12-07 22:35:08 +0530

    Mongodb using ejs

--- Full-Stack/Week3/MongoDB/Practicals/CRUDWithEjs/app.js
+++ Full-Stack/Week3/MongoDB/Practicals/CRUDWithEjs/app.js
@@ -1,14 +1,32 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require("express");
+const app = express();
+const path = require("path");
+const userModel = require("./models/users");
 
-app.set('view engine', 'ejs')
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req,res)=>{
-    res.render("index")
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.listen(3000)
+app.get("/read", async (req, res) => {
+  let allUsers = await userModel.find();
+  console.log("The read is initiated", allUsers)
+  res.render("read", {user: allUsers});
+});
+
+app.post("/create", async (req, res) => {
+  let { name, email, image } = req.body;
+  let createdUser = await userModel.create({
+    name,
+    email,
+    image,
+  });
+  res.redirect("read");
+  console.log(createdUser, "the user is created")
+});
+
+app.listen(3000);
